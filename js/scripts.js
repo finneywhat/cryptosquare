4477b8de62213dc2132781349ad1c5f4aa31a5ef
Chris Finney and Ryan McAlpin <chris@>
2017-02-23 14:58:57 -0800

    add functionality remove all non-letters and return as array of letters

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,18 @@
 // Back-end
 
+var encrypt = function(input) {
 
+  var inputArray = input.toLowerCase().match(/[a-z]/g);
+  console.log(inputArray);
+
+  return result;
+}
 
 $(function() {
   $("form#form").submit(function(event){
     event.preventDefault();
     var userInput = $("#userInput").val();
-    console.log(userInput);
-
+    var result = encrypt(userInput);
+    $("#result").text(result);
   });
 });
